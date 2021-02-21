6847b583e7857d11f9219ef180dd319e5114d4a2
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2021-02-21 19:50:43 +0530

    alternative splitting / unsplitting more amenable to matrix ops

--- rip.recover/R/split.R
+++ rip.recover/R/split.R
@@ -41,27 +41,32 @@ computeSplitRanges <- function(n, patch, overlap)
     list(starts = starts, ends = ends)
 }
 
+## old version
+
+## splitImage <- function(x, patch = 100, overlap = 22)
+##     ## x is expected to be a "rip" object
+##     ## FIXME: allow for multi-channel objects
+## {
+##     x <- as.rip(x)
+##     if (nchannel(x) != 1) stop("Multi-channel images are not yet supported")
+##     ## str(x[], give.attr = FALSE)
+##     patch <- rep(patch, length.out = 2)
+##     overlap <- rep(overlap, length.out = 2)
+##     srow <- computeSplitRanges(nrow(x), patch[1], overlap[1])
+##     scol <- computeSplitRanges(ncol(x), patch[2], overlap[2])
+##     res <- matrix(list(), nrow = length(srow$starts), ncol = length(scol$starts))
+##     for (i in seq_along(srow$starts))
+##         for (j in seq_along(scol$starts))
+##             res[[i, j]] <-
+##                 x[srow$starts[i]:srow$ends[i],
+##                   scol$starts[j]:scol$ends[j]]
+##     attr(res, "split") <- list(row = srow, col = scol)
+##     res
+## }
+
 
 splitImage <- function(x, patch = 100, overlap = 22)
-    ## x is expected to be a "rip" object
-    ## FIXME: allow for multi-channel objects
-{
-    x <- as.rip(x)
-    if (nchannel(x) != 1) stop("Multi-channel images are not yet supported")
-    ## str(x[], give.attr = FALSE)
-    patch <- rep(patch, length.out = 2)
-    overlap <- rep(overlap, length.out = 2)
-    srow <- computeSplitRanges(nrow(x), patch[1], overlap[1])
-    scol <- computeSplitRanges(ncol(x), patch[2], overlap[2])
-    res <- matrix(list(), nrow = length(srow$starts), ncol = length(scol$starts))
-    for (i in seq_along(srow$starts))
-        for (j in seq_along(scol$starts))
-            res[[i, j]] <-
-                x[srow$starts[i]:srow$ends[i],
-                  scol$starts[j]:scol$ends[j]]
-    attr(res, "split") <- list(row = srow, col = scol)
-    res
-}
+    rip.split(x, patch = patch, overlap = overlap, as.array = FALSE)
 
 
 enlargeSplitSizes <- function(s, factor = 1)
@@ -171,4 +176,104 @@ unsplitImage <- function(xsplit, enlarge.factor = 1, full = FALSE)
         }
     return(as.rip(x))
 }
+
+
+## splitting / unsplitting is generally useful for other purposes as
+## well, e.g., to extract patches, process them, and restore
+## them. Assuming that all patches are of the same dimension (possibly
+## overlapping), we can either store them as a list (as above) or a
+## 3-dimensional array. Both have their uses (arrays can do matrix ops
+## easily, lists are easier to resize), and we could think of
+## converting between them.
+
+## For now, the (exported) interface below only implements the 3-D
+## array representation
+
+
+rip.split <- function(x, patch = 8, overlap = 4, as.array = TRUE)
+    ## x is expected to be a "rip" object
+    ## FIXME: allow for multi-channel objects
+{
+    x <- as.rip(x)
+    if (nchannel(x) != 1) stop("Multi-channel images are not yet supported")
+    ## str(x[], give.attr = FALSE)
+    patch <- rep(patch, length.out = 2)
+    overlap <- rep(overlap, length.out = 2)
+    srow <- computeSplitRanges(nrow(x), patch[1], overlap[1])
+    scol <- computeSplitRanges(ncol(x), patch[2], overlap[2])
+    pdim <- c(length(srow$starts), length(scol$starts))
+    if (as.array)
+    {
+        ans <- array(0, dim = c(patch, prod(pdim)))
+        for (j in seq_len(pdim[2]))
+            for (i in seq_len(pdim[1]))
+                ans[, , i + (j - 1L) * pdim[1]] <-
+                    x[srow$starts[i]:srow$ends[i],
+                      scol$starts[j]:scol$ends[j]]
+        class(ans) <- c("rip.split", "array")
+    }
+    else # store as list with dimensions
+    {
+        ans <- matrix(list(), nrow = pdim[1], ncol = pdim[2])
+        for (j in seq_len(pdim[2]))
+            for (i in seq_len(pdim[1]))
+                ans[[i, j]] <-
+                    x[srow$starts[i]:srow$ends[i],
+                      scol$starts[j]:scol$ends[j]]
+        class(ans) <- c("rip.split", "list")
+    }
+    attr(ans, "split") <- list(row = srow, col = scol)
+    ans
+}
+
+
+rip.unsplit <- function(xsplit, ...)
+{
+    if (is.list(xsplit)) unsplitImage(xsplit)
+    else rip.unsplit.array(xsplit, ...)
+}
+
+rip.unsplit.array <- function(xsplit, merge = c("sum", "avg", "weight"))
+{
+    merge <- match.arg(merge) # sum is transpose of the rip.split() operator
+    s <- attr(xsplit, "split")
+    srow <- s$row
+    scol <- s$col
+    if (is.null(s)) stop("Information about split is missing.")
+    pdim <- c(length(srow$starts), length(scol$starts))
+    outdim <- c(tail(srow$ends, 1), tail(scol$ends, 1))
+    x <- matrix(0, outdim[1], outdim[2])
+    if (merge == "weight") stop("weighted merging not implemented yet")
+    if (merge == "avg") # nothing to do for "sum"
+    {
+        ## need to figure out how many values get summed over for each
+        ## destination, and average. We do this by keeping track using
+        ## an auxiliary array
+        xcounts <- matrix(0L, outdim[1], outdim[2])
+    }
+    for (j in seq_len(pdim[2]))
+        for (i in seq_len(pdim[1]))
+        {
+            x[srow$starts[i]:srow$ends[i],
+              scol$starts[j]:scol$ends[j]] <-
+                x[srow$starts[i]:srow$ends[i],
+                  scol$starts[j]:scol$ends[j]] + xsplit[, , i + (j - 1L) * pdim[1]]
+            if (merge == "avg")
+            {
+                xcounts[srow$starts[i]:srow$ends[i],
+                        scol$starts[j]:scol$ends[j]] <-
+                    xcounts[srow$starts[i]:srow$ends[i],
+                            scol$starts[j]:scol$ends[j]] + 1L
+            }
+        }
+    if (merge == "avg") {
+        nzero <- xcounts > 0 # should be all usually
+        x[nzero] <- x[nzero] / xcounts[nzero]
+    }
+    as.rip(x)
+}
+
+
+
+
 
